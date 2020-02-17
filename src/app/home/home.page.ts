e4ac9d1670d8e45a95ba74a78fe6800a0badd555
unknown <mgiovannapcunha@hotmail.com>
2020-02-17 00:15:51 -0300

    versao 1.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,9 +16,9 @@ import { CrudService } from './../service/crud.service';
 export class HomePage implements OnInit {
 
   students: any;
-  studentName: string;
-  studentAge: number;
-  studentAddress: string;
+  studentItem: string;
+  studentQuantidade: number;
+  //studentAddress: string;
 
   constructor(private crudService: CrudService) { }
 
@@ -29,9 +29,9 @@ export class HomePage implements OnInit {
         return {
           id: e.payload.doc.id,
           isEdit: false,
-          Name: e.payload.doc.data()['Name'],
-          Age: e.payload.doc.data()['Age'],
-          Address: e.payload.doc.data()['Address'],
+          Item: e.payload.doc.data()['Item'],
+          Quantidade: e.payload.doc.data()['Quantidade'],
+          //Address: e.payload.doc.data()['Address'],
         };
       })
       console.log(this.students);
@@ -41,13 +41,13 @@ export class HomePage implements OnInit {
 
   CreateRecord() {
     let record = {};
-    record['Name'] = this.studentName;
-    record['Age'] = this.studentAge;
-    record['Address'] = this.studentAddress;
+    record['Item'] = this.studentItem;
+    record['Quantidade'] = this.studentQuantidade;
+    //record['Address'] = this.studentQuantidade;
     this.crudService.create_NewStudent(record).then(resp => {
-      this.studentName = "";
-      this.studentAge = undefined;
-      this.studentAddress = "";
+      this.studentItem = "";
+      this.studentQuantidade = undefined;
+     // this.studentAddress = "";
       console.log(resp);
     })
       .catch(error => {
@@ -61,16 +61,16 @@ export class HomePage implements OnInit {
 
   EditRecord(record) {
     record.isEdit = true;
-    record.EditName = record.Name;
-    record.EditAge = record.Age;
-    record.EditAddress = record.Address;
+    record.EditItem = record.Item;
+    record.EditQuantidade = record.Quantidade;
+    //record.EditAddress = record.Address;
   }
 
   UpdateRecord(recordRow) {
     let record = {};
-    record['Name'] = recordRow.EditName;
-    record['Age'] = recordRow.EditAge;
-    record['Address'] = recordRow.EditAddress;
+    record['Item'] = recordRow.EditItem;
+    record['Quantidade'] = recordRow.EditQuantidade;
+   // record['Address'] = recordRow.EditAddress;
     this.crudService.update_Student(recordRow.id, record);
     recordRow.isEdit = false;
   }
